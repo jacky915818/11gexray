e1512a714504e887af8c441ab906d5a02928a70c
jacky915818 <67189816+jacky915818@users.noreply.github.com>
2022-04-02 17:56:36 +0800

    Update 11geSK5.sh

--- 11geSK5.sh
+++ 11geSK5.sh
@@ -1,9 +1,7 @@
 #/bin/sh
-
 socks_port="1969"
 socks_user="gejia"
 socks_pass="gejia"
-
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
@@ -12,31 +10,26 @@ iptables -t mangle -F
 iptables -F
 iptables -X
 iptables-save
-
 ips=(
 $(hostname -I)
 )
 # Xray Installation
 wget -O /usr/local/bin/xray https://cdn.jsdelivr.net/gh/jacky915818/11gexray/xray
 chmod +x /usr/local/bin/xray
-
 cat <<EOF > /etc/systemd/system/xray.service
 [Unit]
 Description=The Xray Proxy Serve
 After=network-online.target
-
 [Service]
 ExecStart=/usr/local/bin/xray -c /etc/xray/serve.toml
 ExecStop=/bin/kill -s QUIT $MAINPID
 Restart=always
 RestartSec=15s
-
 [Install]
 WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable xray
-
 # Xray Configuration
 mkdir -p /etc/xray
 echo -n "" > /etc/xray/serve.toml
@@ -54,20 +47,16 @@ ip = "${ips[i]}"
 [[inbounds.settings.accounts]]
 user = "$socks_user"
 pass = "$socks_pass"
-
 [[routing.rules]]
 type = "field"
 inboundTag = "$((i+1))"
 outboundTag = "$((i+1))"
-
 [[outbounds]]
 sendThrough = "${ips[i]}"
 protocol = "freedom"
 tag = "$((i+1))"
-
 EOF
 done
-
 systemctl stop xray
 systemctl start xray
 #乐乐QQ：731465465
